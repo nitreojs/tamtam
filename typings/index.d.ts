838a516e905d45c4c749f9575558299529499f29
nitreojs <n1trojscoding@mail.ru>
2019-06-28 21:37:52 +0300

    🔥 v1.0.3-rc.2

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,9 +1,9 @@
 import * as Params from "./params";
-import API from "./components/api";
-import ChainKeyboard from "./chain-keyboard";
-import Keyboard from "./keyboard";
-import Markdown from "./markdown";
-import Updates from "./components/updates";
+import API from "../src/components/api";
+import ChainKeyboard from "../src/chain-keyboard";
+import Keyboard from "../src/keyboard";
+import Markdown from "../src/markdown";
+import Updates from "../src/components/updates";
 
 declare class TamTam {
   public api: API;
